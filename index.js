8c338b8dc007e43af0bc15a29130edee7721143d
levina <lewatron011@gmail.com>
2026-08-30 22:11:22 +0500

    upd

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app.get('/', (req, res) => {
 
 app.post('/api/check', async (req, res) => {
     res.json({piska: "penis"})
-    await pg.query('INSERT INTO users (username, passwordhash) VALUES ($1, $2)' ['akrunik', '4234234'])
+    await pg.query('INSERT INTO users (username, passwordhash) VALUES ($1, $2)', ['akrunik', '4234234'])
 })
