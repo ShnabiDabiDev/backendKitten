0e241d537d2580a82f687ea6d4bcf1279a18dc81
levina <lewatron011@gmail.com>
2026-08-30 21:30:16 +0500

    Update

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 
+app.use(express.json())
+
 app.use(cors({
     origin: "https://designkitten.pages.dev/",
     methods: ["GET", "POST"],
@@ -14,4 +16,8 @@ app.listen(3000, (err) => {
 
 app.get('/', (req, res) => {
     res.redirect('https://designkitten.pages.dev/')
+})
+
+app.post('/api/check', (req, res) => {
+    res.json('penis')
 })
