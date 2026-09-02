ace3a372e47efb541ba13bb767c154bf26c1e976
levina <lewatron011@gmail.com>
2026-09-02 22:27:15 +0500

    up

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const { data, error } = sb.storage
   .from('avatars')
   .list();
 
-console.log(data, error + "Update");
+console.log(data, error + "Update all");
 
 const io = new Server(server, {
     cors: {
