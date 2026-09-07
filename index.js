a75d36e8e91dcccdfbbc0c004923c3fac6627c13
levina <lewatron011@gmail.com>
2026-09-07 20:22:15 +0500

    update

--- index.js
+++ index.js
@@ -56,19 +56,19 @@ app.post('/api/check', async (req, res) => {
     // await pg.query('INSERT INTO users (username, passwordhash) VALUES ($1, $2)', ['akrunik', '4234234'])
 })
 
-app.post('/api/upload-avatar', upload.single('avatar'), async (req, res) => {
+app.post('/api/uploadavatar', upload.single('avatar'), async (req, res) => {
    const file = req.file
    const storage = sb.storage
    const bucket = storage.from('avatars')
 
    res.json({
-        success: true,
+        // success: true,
         message: 'Файл получен!',
-        fileInfo: {
-            name: file.originalname,
-            size: file.size,
-            type: file.mimetype
-        }
+        // fileInfo: {
+        //     name: file.originalname,
+        //     size: file.size,
+        //     type: file.mimetype
+        // }
     });
 })
 
